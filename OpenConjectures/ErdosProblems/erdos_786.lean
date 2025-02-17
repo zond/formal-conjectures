fed8723fe5b1c040d4fc56cbd4a85d24583b008f
Salvatore Mercuri <mercuris@google.com>
2025-02-17 18:35:57 +0100

    Add formalisation of `erdos_786`
    PiperOrigin-RevId: 727888503
    Change-Id: I72a7088d064cd0b4b31b192dd7b589f37dba5246

---
+++ OpenConjectures/ErdosProblems/erdos_786.lean
@@ -0,0 +1,95 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+-- Erdos Problems URL: https://www.erdosproblems.com/786
+import OpenConjectures.Util.ProblemImports
+import GDMLib.ProblemImports
+
+open Filter
+
+open scoped Topology
+
+/--
+`Nat.IsMulCardSet A` means that `A` is a set of natural numbers that
+satisfies the property that $a_1\cdots a_r = b_1\cdots b_s$ with $a_i, b_j\in A$
+can only hold when $r = s$.
+-/
+def Set.IsMulCardSet {α : Type*} [CommMonoid α] (A : Set α) :=
+  ∀ (a b : Finset α) (_ :↑a ⊆ A) (_ : ↑b ⊆ A) (_ : a.prod id = b.prod id),
+    a.card = b.card
+
+/--
+Let $\epsilon > 0$. Is there some set $A\subset\mathbb{N}$ of density $> 1 - \epsilon$
+such that $a_1\cdots a_r = b_1\cdots b_s$ with $a_i, b_j\in A$ can only hold when
+$r = s$?
+-/
+@[open_problem]
+theorem erdos_786.parts.i (ε δ : ℝ) (hε : 0 < ε ∧ ε ≤ 1) (hδ : 1 - ε < δ) :
+    ∃ (A : Set ℕ), 0 ∉ A ∧ A.HasDensity δ ∧
+      A.IsMulCardSet :=
+  sorry
+
+/--
+Let $\epsilon > 0$. Is there some set $A\subset\{1, ..., N\}$ of density
+$> 1 - \epsilon$ and size $\geq (1 - o(1))N$ such that
+$a_1\cdots a_r = b_1\cdots b_s$ with $a_i, b_j\in A$ can only hold when
+$r = s$?
+-/
+@[open_problem]
+theorem erdos_786.parts.ii (ε δ : ℝ) (hε : 0 < ε ∧ ε ≤ 1) (hδ : 1 - ε < δ) :
+    ∃ (N : ℕ) (A : ℕ → Set ℕ) (f : ℕ → ℝ) (_ : Tendsto f atTop (𝓝 0)),
+      ∀ N, A N ⊆ Set.Icc 1 N ∧ (1 - f N) * N ≤ (A N).ncard ∧
+        (A N).HasDensity δ ∧
+          (A N).IsMulCardSet :=
+  sorry
+
+/--
+An example of such a set with density $1/4$ is given by the integers $\equiv 2\pmod{4}$
+-/
+theorem erdos_786.parts.i.example (A : Set ℕ) (hA : A = { n | n % 4 = 2 }) :
+    A.HasDensity (1 / 4) ∧
+      A.IsMulCardSet :=
+  sorry
+
+/--
+`consecutivePrimes p` asserts that `p` is a strictly increasing finite sequences of
+consecutive primes.
+-/
+def consecutivePrimes {k : ℕ} (p : Fin k.succ → ℕ) :=
+    ∀ i, (p i).Prime ∧ StrictMono p ∧
+      ∀ i < k, ∀ m ∈ Set.Ioo (p i) (p (i + 1)), ¬m.Prime
+
+-- Reworded slightly from the link.
+/--
+Let $\epsilon > 0$ be given. Then, for a sequence of sufficiently large
+consecutive primes $p_1 < \cdots < p_k$ such that
+$$
+\sum_{i=1}^k \frac{1}{p_i} < 1 < \sum_{i=1}^{k + 1} \frac{1}{p_i},
+$$
+the set $A$ of all naturals divisible by exactly one of $p_1, ..., p_k$ has
+density $1 / e - \epsilon$ and has the property that $a_1\cdots a_r = b_1\cdots b_s$
+with $a_i, b_j\in A$ can only hold when $r = s$.
+-/
+theorem erdos_786.parts.i.selfridge (ε : ℝ) (hε : 0 < ε ∧ ε ≤ 1) :
+    -- TODO(mercuris) : I think we want `k` to be allowed to vary somehow as well, but maybe the exists is sufficient
+    ∃ (k : ℕ),
+      -- Sufficient to take L^∞ norm to guarantee all primes are large, due to the consecutivePrimes assertion
+      ∀ᶠ (p : Fin k.succ → ℕ) in atTop, consecutivePrimes p ∧
+        ∑ i in Finset.univ.filter (· < Fin.last _), (1 : ℝ) / p i < 1 ∧
+          1 < ∑ i, (1 : ℝ) / p i →
+    { n | ∃! i < k, p i ∣ n }.HasDensity (1 / Real.exp 1 - ε) ∧
+      { n | ∃! i < k, p i ∣ n }.IsMulCardSet :=
+  sorry

4230b2fe894b5de7aff4b8ea30f826887d075f44
Mirek Olšák <miroslavo@google.com>
2025-02-18 20:04:27 +0100

    Add Determinantal Conjecture
    PiperOrigin-RevId: 728281047
    Change-Id: I42063891c477cfd6f1f9b5fd6f9df5e55aa1c76b

---
+++ OpenConjectures/Wikipedia/determinantal_conjecture.lean
@@ -0,0 +1,41 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+-- https://en.wikipedia.org/wiki/Determinantal_conjecture
+import OpenConjectures.Util.ProblemImports
+
+/-
+Determinantal Conjecture asks whether the determinant of a sum $A + B$
+of two $n$ by $n$ normal complex matrices A and B lies in the convex hull
+of the n! points $\prod i (\lambda(A)_i + \lambda(B)_{σ(i)})$,
+where the numbers $\lambda (A)_i$ and $\lambda (B)_i$ are
+the eigenvalues of $A$ and $B$, and $\sigma$ is an element of the symmetric
+group $S_n$.
+
+Here, we represent the two normal matrices as $U_1 D_1 U_1^*$ and
+$U_2 D_2 U_2^*$ respectively, where $U_1, U_2$ are unitary and $D_1, D_2$
+are diagonal. This is an universal form of a normal matrix,
+allowing to retrieve $\lambda (A)_i$ as $D_1_{i,i}$, whereas the current mathlib
+doesn't support obtaining the vector of eigenvalues from a general normal
+non-Hermitian matrix.
+-/
+@[open_problem]
+theorem determinantal_conjecture
+    (n : Type) [Fintype n] [DecidableEq n]
+    (d1 d2 : n → ℂ) (U1 U2 : unitary (Matrix n n ℂ)) :
+    (U1 * Matrix.diagonal d1 * star U1 + U2 * Matrix.diagonal d2 * star U2).det
+      ∈ convexHull ℝ { ∏ i, (d1 i + d2 (σ i)) | σ : Equiv.Perm n } :=
+  sorry

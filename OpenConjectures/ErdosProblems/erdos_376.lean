9d36d9a454df5790e2e533cda509a6959235f63c
Salvatore Mercuri <mercuris@google.com>
2025-02-27 08:56:05 +0100

    Formalisation of `erdos_376`
    PiperOrigin-RevId: 731604681
    Change-Id: If5d4514351ba47aff4f30f66c1f7414096852a7f

---
+++ OpenConjectures/ErdosProblems/erdos_376.lean
@@ -0,0 +1,37 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+-- Erdos Problems URL: https://www.erdosproblems.com/376
+import OpenConjectures.Util.ProblemImports
+
+/--
+Are there infinitely many $n$ such that ${2n\choose n}$ is
+coprime to $105$?
+-/
+@[problem_status open]
+theorem erdos_376 :
+    { n | ((2 * n).choose n).Coprime 105 }.Infinite :=
+  sorry
+
+/--
+Erd\"{o}s, Graham, Ruzsa, and Straus [EGRS75] have shown that, for any two odd primes $p$ and $q$, there are infinite many $n$ such
+that ${2n\choose n}$ is coprime to $pq$.
+-/
+@[problem_status solved]
+theorem erdos_376.variants.prime {p q : ℕ} (h₁ : p.Prime)
+    (h₂ : Odd p) (h₃ : q.Prime) (h₄ : Odd q) :
+    { n | ((2 * n).choose n).Coprime (p * q) }.Infinite :=
+  sorry

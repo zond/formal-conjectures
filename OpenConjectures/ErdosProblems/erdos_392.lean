cc83a6c7a71d831ebc508936ab30bd4bd8cafc99
Salvatore Mercuri <mercuris@google.com>
2025-03-03 11:28:04 +0100

    Add formalisation of `erdos_392`
    PiperOrigin-RevId: 732843531
    Change-Id: Ife8dde547f4cd89837b96304ec311a78b07fa292

---
+++ OpenConjectures/ErdosProblems/erdos_392.lean
@@ -0,0 +1,54 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+-- Erdos Problems URL: https://www.erdosproblems.com/392
+import OpenConjectures.Util.ProblemImports
+
+open Filter
+
+open scoped Nat
+
+/--
+Let $A(n)$ denote the least value of $t$ such that
+$$
+  n! = a_1 \cdots a_t
+$$
+with $a_1 \leq \cdots \leq a_t\leq n^2$. Is it true that
+$$
+  A(n) = \frac{n}{2} - \frac{n}{2\log n} + o\left(\frac{n}{\log n}\right)?
+$$
+-/
+@[problem_status open]
+theorem erdos_392 (A : ℕ → ℕ)
+    (hA : ∀ n, IsLeast
+      { t + 1 | (t) (_ : ∃ a : Fin (t + 1) → ℕ, (n)! = ∏ i, a i ∧ Monotone a ∧ a t ≤ n ^ 2) }
+      (A n)) :
+    (fun (n : ℕ) => (A n - n / 2 + n / (2 * Real.log n) : ℝ)) =o[atTop] fun n => n / Real.log n :=
+  sorry
+
+/--
+If we change the condition to $a_t \leq n$ it can be shown that
+$$
+  A(n) = n - \frac{n}{log n} + o\left(\frac{n}{\log n}\right)
+$$
+-/
+@[problem_status solved]
+theorem erdos_392.variants.lower (A : ℕ → ℕ)
+    (hA : ∀ n, IsLeast
+      { t + 1 | (t) (_ : ∃ a : Fin (t + 1) → ℕ, (n)! = ∏ i, a i ∧ Monotone a ∧ a t ≤ n) }
+      (A n)) :
+    (fun (n : ℕ) => (A n - n + n / Real.log n : ℝ)) =o[atTop] fun n => n / Real.log n :=
+  sorry

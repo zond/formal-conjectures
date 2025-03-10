c8bd59bad97c0109674ff1f7aae3c7aabfe3b324
Paul Lezeau <lezeau@google.com>
2025-03-10 13:10:53 +0100

    Add formalisation of erdos_10
    PiperOrigin-RevId: 735334718
    Change-Id: I5519177c00869a36fa2ac267d0ae2c5b2f92bd4f

---
+++ OpenConjectures/ErdosProblems/erdos_10.lean
@@ -0,0 +1,92 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+--Erdos Problem URL: https://www.erdosproblems.com/10
+import OpenConjectures.Util.ProblemImports
+
+/--
+The lower asymptotic density of a set `S` is the liminf as `n → ∞` of the
+ratio `|{a ∈ S | a < n}| / n`
+-/
+noncomputable def lowerDensity (S : Set ℕ) : ℝ :=
+    Filter.atTop.liminf (fun n => (Set.Iio n ∩ S).ncard / n)
+
+/--
+The set of natural numbers that can be written as a sum
+of a prime and at most `k` powers of 2.
+-/
+abbrev sumPrimeAndTwoPows (k : ℕ) : Set ℕ :=
+  { p + (pows.map (2 ^ ·)).sum | (p : ℕ) (pows : Multiset ℕ) (_ : p.Prime)
+    (_ : pows.card ≤ k)}
+
+/--
+Is there some `k` such that every integer is the sum of a prime and at most `k`
+ powers of `2`?
+-/
+@[problem_status open]
+theorem erdos_10 : ∃ k, sumPrimeAndTwoPows k = Set.univ \ {0, 1} := by
+  sorry
+
+/--
+Gallagher [Ga75] has shown that for any `ϵ>0` there exists `k(ϵ)`
+such that the set of integers which are the sum of a prime and at most `k(ϵ)`
+many powers of 2 has lower density at least `1−ϵ`.
+
+Ref: Gallagher, P. X., _Primes and powers of 2_.
+-/
+@[problem_status solved]
+theorem erdos_10.variants.gallagher (ε : ℝ)
+    (hε : 0 < ε) : ∃ k, 1 - ε ≤ lowerDensity (sumPrimeAndTwoPows k) := by
+  sorry
+
+/--
+Granville and Soundararajan [GrSo98] have conjectured that at most 3
+powers of 2 suffice for all odd integers, and hence at most 4 powers of 2
+suffice for all even integers.
+
+Ref: Granville, A. and Soundararajan, K., _A Binary Additive Problem of Erdős and the Order of $2$ mod $p^2$_
+-/
+@[problem_status open]
+theorem erdos_10.variants.granville_soundararajan_odd :
+    {n : ℕ | Odd n ∧ 1 < n} ⊆ sumPrimeAndTwoPows 3 ∧
+      {n : ℕ | Even n ∧ n ≠ 0} ⊆ sumPrimeAndTwoPows 4 := by
+  sorry
+
+/--
+Bogdan Grechuk has observed that `1117175146` is not the sum of a prime
+and at most 3  powers of 2.
+-/
+@[problem_status solved]
+theorem erdos_10.variants.grechuk_example :
+    1117175146 ∉ sumPrimeAndTwoPows 3 := by
+  sorry
+
+/--
+There are infinitely many even integers not the sum of a prime and 2 powers of 2
+-/
+@[problem_status solved]
+theorem erdos_10.variants.two_pows :
+    Set.Infinite <| {n : ℕ | Even n} \ sumPrimeAndTwoPows 2 := by
+  sorry
+
+/--
+Bogdan Grechuk has observed that 1117175146 is not the sum of a prime and at most 3
+powers of 2, and pointed out that parity considerations, coupled with the fact that there are many integers not the sum of a prime and 2 powers of 2 suggest that there exist infinitely many even integers which are not the sum of a prime and at most 3 powers of 2).
+-/
+@[problem_status open]
+theorem erdos_10.variants.gretchuk :
+    Set.Infinite <| {n : ℕ | Even n} \ sumPrimeAndTwoPows 3 := by
+  sorry

032c84db54b51c41217c46e0ea090e0069cefdee
Salvatore Mercuri <mercuris@google.com>
2025-03-11 09:09:18 +0100

    Formalisation of `erdos_126`
    PiperOrigin-RevId: 735662014
    Change-Id: I7ef20453317924cb4018db81739b1ee91ee9ab97

---
+++ OpenConjectures/ErdosProblems/erdos_126.lean
@@ -0,0 +1,62 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+-- Erdos Problems URL: https://www.erdosproblems.com/126
+import OpenConjectures.Util.ProblemImports
+
+open Filter
+
+def IsMaximalAddFactorsCard (f : ℕ → ℕ) : Prop := ∀ n,
+    IsGreatest
+      { m | ∀ (A : Finset ℕ), A.card = n →
+        m ≤ (∏ ⟨a, b⟩ ∈ A.offDiag, (a + b)).primeFactors.card}
+      (f n)
+
+/--
+Let $f(n)$ be maximal such that if $A\subseteq\mathbb{N}$ has $|A| = n$ then
+$\prod_{a\neq b\in A}(a + b)$ has at least $f(n)$ distinct prime factors.
+Is it true that $f(n)/\log n \to\infty$?
+-/
+@[problem_status open]
+theorem erdos_126
+    (f : ℕ → ℕ)
+    (hf : IsMaximalAddFactorsCard f) :
+    Tendsto (fun n => f n / Real.log n) atTop atTop :=
+  sorry
+
+/--
+Erd\"{o}s and Tur\'{a}n proved [ErTu34] in their first joint paper that
+$$
+  \log n \ll f(n) \ll n/\log n
+$$
+
+[ErTu34] Erdős, Paul and Turan, Paul, _On a Problem in the Elementary Theory of Numbers_. Amer. Math. Monthly (1934), 608-611.
+-/
+theorem erdos_126.variants.IsBigO
+    (f : ℕ → ℕ)
+    (hf : IsMaximalAddFactorsCard f) :
+    ((fun (n : ℕ) => Real.log n) =O[atTop] fun (n : ℕ) => (f n : ℝ)) ∧
+      (fun (n : ℕ) => (f n : ℝ)) =O[atTop] fun (n : ℕ) => n / Real.log n := sorry
+
+/--
+Erd\"{o}s says that $f(n) = o(n / \log n)$ has never been proved.
+-/
+@[problem_status open]
+theorem erdos_126.variants.isLittleO
+    (f : ℕ → ℕ)
+    (hf : IsMaximalAddFactorsCard f) :
+    (fun (n : ℕ) => (f n : ℝ)) =o[atTop] (fun (n : ℕ) => n / Real.log n) :=
+  sorry

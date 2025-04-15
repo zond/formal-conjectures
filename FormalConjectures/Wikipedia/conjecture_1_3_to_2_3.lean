a222a685df35e4da92f37f35869e9707055107e5
Moritz Firsching <firsching@google.com>
2025-04-15 14:52:55 +0200

    Change type of `total_ext` to match the intented meaning
    Previously we have used OrderEmbedding here:
    
    An order embedding is an embedding `f : α ↪ β` such that `a ≤ b ↔ (f a) ≤ (f b)`. This definition is an abbreviation of `RelEmbedding (≤) (≤)`.
    
    This is too strict: only `a ≤ b → (f a) ≤ (f b)` needs to hold
    
    PiperOrigin-RevId: 747839024
    Change-Id: I4d0f94a9a70cf9de696220ee3d023b3b9f7a2b2d

--- FormalConjectures/Wikipedia/conjecture_1_3_to_2_3.lean
+++ FormalConjectures/Wikipedia/conjecture_1_3_to_2_3.lean
@@ -22,12 +22,12 @@ Does every finite partially ordered set that is not totally ordered
 contain two elements x and y such that the probability that
 x appears before y in a random linear extension is between 1/3 and 2/3?
 
-The set of all total order extensions is represented here
-as monotone bijections P -> 1..n
+The set of all total order extensions is represented as order preserving
+bijections P -> 1..n.
 -/
 @[problem_status open]
 theorem conjecture_1_3_to_2_3 (P : Type) [Finite P] [PartialOrder P]
-    (not_total : ¬ IsTotal P (· ≤ ·)) (total_ext : Set (P ↪o ℕ))
+    (not_total : ¬ IsTotal P (· ≤ ·)) (total_ext : Set <| OrderHom P ℕ)
     (total_ext_def : ∀ σ, σ ∈ total_ext ↔ Set.range σ = Set.Icc 1 (Nat.card P)) :
     ∃ x y : P, ({σ ∈ total_ext | σ x < σ y}.ncard / total_ext.ncard : ℚ)
       ∈ Set.Icc (1/3) (2/3) := sorry

889c5d5eaa9243afc7beeddbdd7059c2f6067c29
Moritz Firsching <firsching@google.com>
2025-04-15 09:36:59 +0200

    `lcm` instead of `gdc` in `F`
    A typo: the informal text clearly requires least common multiple not the greatest common divisor.
    
    PiperOrigin-RevId: 747749960
    Change-Id: Ida6dffdb75615fa158155109d654932c9bb22141

--- FormalConjectures/ErdosProblems/873.lean
+++ FormalConjectures/ErdosProblems/873.lean
@@ -17,10 +17,11 @@ limitations under the License.
 -- Erdos Problems URL: https://www.erdosproblems.com/873
 import FormalConjectures.Util.ProblemImports
 
-/--Let `a` be some sequence of natural numbers. We set `F(A,X,k)` to be the number of `i` such that
-`[a_i,a_{i+1}, … ,a_{i+k−1}] < X`, where the left-hand side is the least common multiple.-/
+/--Let `a` be some sequence of natural numbers. We set `F(A,X,k)` to be the count of
+the number of `i` such that `[a_i,a_{i+1}, … ,a_{i+k−1}] < X`,
+where the left-hand side is the least common multiple.-/
 noncomputable abbrev F (a : ℕ → ℕ) (X : ℝ) (k : ℕ) : ℕ∞ :=
-  {i : ℕ | (Finset.range k).gcd (fun m => a (i + m)) < X}.encard
+  {i : ℕ | (Finset.range k).lcm (fun m => a (i + m)) < X}.encard
 
 /--Let `A={a_1 < a_2 <⋯} ⊆ ℕ` and let `F(A,X,k)` count the number of `i` such that
 `[a_i,a_{i+1}, … ,a_{i+k−1}] < X`, where the left-hand side is the least common multiple.

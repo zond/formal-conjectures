667669b774ffe77354c7a80e7a6262151d3e02d1
Formal Conjectures Team <formal-conjectures@google.com>
2025-04-16 09:54:28 +0200

    Formalization of erdos 316
    PiperOrigin-RevId: 748192373
    Change-Id: I8a953d082bf01f98746c6f42272af848afef3230

---
+++ FormalConjectures/ErdosProblems/316.lean
@@ -0,0 +1,44 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+-- Erdos Problems URL: https://www.erdosproblems.com/316
+import FormalConjectures.Util.ProblemImports
+
+/-- Is it true that if $A \subseteq \mathbb{N}∖{1}$ is a finite set with $\sum_{n \in A} \frac{1}{n} < 2$ then there is a partition $A=A_1 \sqcup A_2$ such that $\sum_{n \in A_i} \frac{1}{n} < 1$ for $i=1,2$?
+
+Solution: False. -/
+@[problem_status solved]
+theorem erdos_316 : ∃ A : Finset ℕ, 0 ∉ A ∧ 1 ∉ A ∧
+  ∑ n ∈ A, (1 / n : ℚ) < 2 ∧ ∀ (A₁ A₂ : Finset ℕ),
+    Disjoint A₁ A₂ → A = A₁ ∪ A₂ →
+    1 ≤ ∑ n ∈ A₁, (1 / n : ℚ) ∨ 1 ≤ ∑ n ∈ A₂, (1 / n : ℚ) := by
+  sorry
+
+/-- It is not true if `A` is a multiset (easier) -/
+lemma erdos_316.variants.multiset : ∃ A : Multiset ℕ, 0 ∉ A ∧ 1 ∉ A ∧
+  (A.map ((1 : ℚ) / ·)).sum < 2 ∧ ∀ (A₁ A₂ : Multiset ℕ),
+    Disjoint A₁ A₂ → A = A₁ ∪ A₂ →
+    1 ≤ (A₁.map ((1 : ℚ) / ·)).sum∨ 1 ≤ (A₂.map ((1 : ℚ) / ·)).sum := by
+  sorry
+
+
+/-- More generally, Sándor shows that for any n≥2 there exists a finite set $A \subseteq \mathbb{N}∖{1}$
+with $\sum_{n \in A} \frac{1}{k} < n$ , and no partition into n parts each of which has $\sum_{n \in A_i} \frac{1}{k} < 1$. -/
+@[problem_status solved]
+theorem erdos_316.variants.generalized (n : ℕ) (hn : 2 ≤ n) : ∃ A : Finset ℕ,
+  0 ∉ A ∧ 1 ∉ A ∧ ∑ k ∈ A, (1 / k : ℚ) < n ∧ ∀ P : Finpartition A,
+    P.parts.card = n → ∃ p ∈ P.parts, 1 ≤ ∑ n ∈ p, (1 / n : ℚ) := by
+  sorry

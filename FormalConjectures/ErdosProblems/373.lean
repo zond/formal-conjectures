7aa86c6c76c4aeaace42f2345386a714025d2e9e
Salvatore Mercuri <mercuris@google.com>
2025-04-23 17:48:56 +0200

    The assumption that `P(n(n - 1)) > 4log n`, where `P(n)` is the largest prime factor, is not true in general. For `n = 4`, we get `3 = P(12) > 4log 4` which is false.
    The erdos problems website is not so precise on this variant, but the reference (Theorem 2 of https://users.renyi.hu/~p_erdos/1976-39.pdf) shows that the assumption need only hold for sufficiently large `n`
    
    PiperOrigin-RevId: 750607749
    Change-Id: I9b25c1d94e896a7758f8a4e713936ed7685f9fe3

--- FormalConjectures/ErdosProblems/373.lean
+++ FormalConjectures/ErdosProblems/373.lean
@@ -17,27 +17,27 @@ limitations under the License.
 -- Erdős Problems URL: https://www.erdosproblems.com/373
 import FormalConjectures.Util.ProblemImports
 
-
 open scoped Nat
 /--
-Let `S` be the set of non-trivial solutions to the equation `n! = a₁! ··· aₖ!` such that `a₁ ≥ ... ≥ aₖ` and `n-1 > a₁`.
+Let `S` be the set of non-trivial solutions to the equation `n! = a₁! ··· aₖ!`
+such that `a₁ ≥ ... ≥ aₖ` and `n-1 > a₁`.
 -/
 abbrev S : Set (ℕ × List ℕ) :=
   {(n, l) | n ! = (l.map Nat.factorial).prod ∧ l.Sorted (· ≥ ·)
     ∧ l.headI < (n - 1 : ℕ) ∧ ∀ a ∈ l, 1 < a }
 
-
 /--
-Show that the equation `n!=a_1!a_2!···a_k!`, with `n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, has only finitely many solutions.
+Show that the equation `n!=a_1!a_2!···a_k!`, with `n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, has
+only finitely many solutions.
 -/
 @[category research open]
 theorem erdos_373 : S.Finite := by
   sorry
 
-
 /--
 Show that if `P(n(n+1)) / log n → ∞` where `P(m)` denotes the largest prime factor of `m`, then
-the equation `n!=a_1!a_2!···a_k!`, with `n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, has only finitely many solutions.
+the equation `n!=a_1!a_2!···a_k!`, with `n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, has only
+finitely many solutions.
 -/
 @[category research solved]
 theorem erdos_373.variants.of_limit
@@ -45,27 +45,31 @@ theorem erdos_373.variants.of_limit
     S.Finite := by
   sorry
 
-
+-- Formalisation note: at the time of writing, the website states "Erdős proved that this problem
+-- would also follow from showing that $P(n(n - 1)) > 4\log n$". This is slightly unclear
+-- as to which $n$ is meant here, as for example the inequality fails for $n = 4$.
+-- The referenced material (Theorem 2 of https://users.renyi.hu/~p_erdos/1976-39.pdf), shows
+-- that no non-trivial solutions hold for any `n` with `n > n_0` and `P(n(n - 1)) > 4 log n`.
+-- So for finiteness, it is enough to assume the inequality holds for sufficiently large `n`.
 /--
-Show that if `P(n(n−1)) > 4 log n` where `P(m)` denotes the largest prime factor of `m`, then
-the equation `n!=a_1!a_2!···a_k!`, with `n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, has only finitely many solutions.
+Show that if `P(n(n−1)) > 4 log n` for large enough `n`, where `P(m)` denotes the
+largest prime factor of `m`, then the equation `n!=a_1!a_2!···a_k!`, with
+`n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, has only finitely many solutions.
 -/
 @[category research solved]
 theorem erdos_373.variants.of_lower_bound
-    (H : ∀ n > (0 : ℕ), 4*(n : ℝ).log < (n*(n-1 : ℕ)).maxPrimeFac) :
+    (H : ∀ᶠ (n : ℕ) in Filter.atTop, 4*(n : ℝ).log < (n*(n-1 : ℕ)).maxPrimeFac) :
     S.Finite := by
   sorry
 
-
 /--
-Hickerson conjectured the largest solution the equation `n!=a_1!a_2!···a_k!`, with `n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`,
-is `16!=14!5!2!`.
+Hickerson conjectured the largest solution the equation `n!=a_1!a_2!···a_k!`, with
+`n−1 > a_1 ≥ a_2 ≥ ··· ≥ a_k`, is `16!=14!5!2!`.
 -/
 @[category research open]
 theorem erdos_373.variants.maximal_solution :
     (16, [14, 5, 2]) ∈ S ∧ ∀ s ∈ S, s.fst ≤ 16 := by
   sorry
-
 
 /--
 Surányi was the first to conjecture that the only non-trivial solution to `a!b!=n!`

cb78bea2e5ce2360f8723b4e18e55210de9422e7
Salvatore Mercuri <mercuris@google.com>
2025-05-02 19:13:16 +0200

    Hypotheses for several results in `409.lean` contain contradictions whenever `n` never reaches a prime under iteration of the relevant function. For example, `sigma 1 0` is always `0`, and therefore never reaches a prime. Re-formalise all results in the file to make use of existential quantifier to fix.
    Also, fix hypotheses in two sigma variants which still references `totient` and general style fixes.
    
    PiperOrigin-RevId: 754046600
    Change-Id: I911782664ce974014a69bfa5c15ffa1cf781fd56

--- FormalConjectures/ErdosProblems/409.lean
+++ FormalConjectures/ErdosProblems/409.lean
@@ -21,68 +21,141 @@ import FormalConjectures.Util.ProblemImports
 
 *Reference:* [erdosproblems.com/409](https://www.erdosproblems.com/409)
 -/
-open scoped Topology ArithmeticFunction
+open scoped Topology ArithmeticFunction Nat
+open Filter
 
 /--
 How many iterations of $n\mapsto\phi(n) + 1$ are needed before a prime is reached?
 -/
+-- Formalisation note: the sequence of iterates always terminates if `n > 0`
+-- since it is strictly decreasing unless the input is prime, at which point
+-- it becomes static. See also https://oeis.org/A039651
 @[category research open, AMS 11]
-theorem erdos_409.parts.i (n : ℕ) :
-    IsLeast { i | (fun j => j.totient + 1)^[i] n |>.Prime } answer(sorry) :=
+theorem erdos_409.parts.i (n : ℕ) (hn : 0 < n) :
+    IsLeast { i | (φ · + 1)^[i] n |>.Prime } answer(sorry) :=
+  sorry
+
+/-- If $n > 0$, then the iteration $n\mapsto\phi(n) + 1$ necessarily
+reaches a prime. -/
+@[category test]
+theorem erdos_409.termination (n : ℕ) (hn : 0 < n) :
+    ∃ i, (φ · + 1)^[i] n |>.Prime :=
+  sorry
+
+-- Formalisation note: it's possible that solution to `erdos_409.parts.i` needs to be
+-- expressed asymptotically. To handle this we include `IsTheta`, `IsBigO`
+-- and `IsLittleO` variants below. Since a solution is not known this necessitates
+-- the use of an `answer(sorry)` placeholder. Trivial or sub-optimal solutions
+-- will therefore exist to the asymptotic formalisations. A true solution to
+-- the asymptotic variants should have a degree of optimality or non-triviality to it.
+/--
+Let $c(n)$ be the minimum number of iterations of $n\mapsto\phi(n) + 1$ before a prime
+is reached. What is $\Theta(c(n))$?
+-/
+@[category research open, AMS 11]
+theorem erdos_409.parts.i.variants.isTheta (c : ℕ → ℕ)
+    (h : ∀ n > 0, IsLeast { i | (φ · + 1)^[i] n |>.Prime } (c n)) :
+    (fun n => (c n : ℝ)) =Θ[atTop] (answer(sorry) : ℕ → ℝ) :=
+  sorry
+
+/--
+Let $c(n)$ be the minimum number of iterations of $n\mapsto\phi(n) + 1$ before a prime
+is reached. Find the simplest function $g(n)$ such that $c(n) = O(g(n))$?
+-/
+@[category research open, AMS 11]
+theorem erdos_409.parts.i.variants.isBigO (c : ℕ → ℕ)
+    (h : ∀ n > 0, IsLeast { i | (φ · + 1)^[i] n |>.Prime } (c n)) :
+    (fun n => (c n : ℝ)) =O[atTop] (answer(sorry) : ℕ → ℝ) :=
+  sorry
+
+/--
+Let $c(n)$ be the minimum number of iterations of $n\mapsto\phi(n) + 1$ before a prime
+is reached. Find the simplest function $g(n)$ such that $c(n) = o(g(n))$?
+-/
+@[category research open, AMS 11]
+theorem erdos_409.parts.i.variants.isLittleO (c : ℕ → ℕ)
+    (h : ∀ n > 0, IsLeast { i | (φ · + 1)^[i] n |>.Prime } (c n)) :
+    (fun n => (c n : ℝ)) =o[atTop] (answer(sorry) : ℕ → ℝ) :=
   sorry
 
 /--
 Can infinitely many $n$ reach the same prime under the iteration $n\mapsto\phi(n) + 1$?
 -/
 @[category research open, AMS 11]
-theorem erdos_409.parts.ii
-    (iterEnd : ℕ → ℕ)
-    (iterEnd_spec: ∀ n, IsLeast { i | (fun j => j.totient + 1)^[i] n |>.Prime } (iterEnd n)) :
-    ∃ (p : ℕ) (hp : p.Prime),
-      { n | (fun j => j.totient + 1)^[iterEnd n] n = p }.Infinite :=
+theorem erdos_409.parts.ii :
+    ∃ (p : ℕ) (hp : p.Prime), { n | ∃ i, (φ · + 1)^[i] n = p }.Infinite :=
   sorry
 
 /--
 What is the density of $n$ which reach any fixed prime under the iteration $n\mapsto\phi(n) + 1$?
 -/
 @[category research open, AMS 11]
-theorem erdos_409.parts.iii (p : ℕ)
-    (h : p.Prime)
-    (iterEnd : ℕ → ℕ)
-    (iterEnd_spec: ∀ n, IsLeast { i | (fun j => j.totient + 1)^[i] n |>.Prime } (iterEnd n))
-    (α : ℝ)
-    (hα : { n | (fun j => j.totient + 1)^[iterEnd n] n = p }.HasDensity α) :
+theorem erdos_409.parts.iii (p : ℕ) (h : p.Prime) (α : ℝ)
+    (hα : { n | ∃ i, (φ · + 1)^[i] n = p }.HasDensity α) :
     α = answer(sorry) :=
   sorry
 
 /--
 How many iterations of $n\mapsto\sigma(n) - 1$ are needed before a prime is reached?
 -/
+-- Formalisation note: non-termination of this sequence is less clear since
+-- it is strictly increasing except at primes.
 @[category research open, AMS 11]
-theorem erdos_409.variants.sigma.parts.i (n : ℕ) :
-    IsLeast { i | (fun j => (σ 1 j - 1 : ℕ))^[i] n |>.Prime } answer(sorry) :=
+theorem erdos_409.variants.sigma.parts.i (n : ℕ) (hn : n > 1) :
+    IsLeast { i | (σ 1 · - 1)^[i] n |>.Prime } answer(sorry) :=
+  sorry
+
+/-- If $n > 1$ then the iteration $n\mapsto\sigma(n) - 1$ necessarily reaches a prime. -/
+@[category test]
+theorem erdos_409.variants.sigma.termination (n : ℕ) (hn : n > 1) :
+    ∃ i, (σ 1 · - 1)^[i] n |>.Prime :=
+  sorry
+
+-- Formalisation note: See the above formalisation note for the rationale
+-- for including asymptotic variants
+/--
+Let $c(n)$ be the minimum number of iterations of $n\mapsto\sigma(n) - 1$ before a prime
+is reached. What is $\Theta(c(n))$?
+-/
+@[category research open, AMS 11]
+theorem erdos_409.variants.sigma.parts.i.isTheta (c : ℕ → ℕ)
+    (h : ∀ n > 1, IsLeast { i | (σ 1 · - 1)^[i] n |>.Prime } (c n)) :
+    (fun n => (c n : ℝ)) =Θ[atTop] (answer(sorry) : ℕ → ℝ) :=
+  sorry
+
+/--
+Let $c(n)$ be the minimum number of iterations of $n\mapsto\sigma(n) - 1$ before a prime
+is reached. Find the simplest function $g(n)$ such that $c(n) = O(g(n))$?
+-/
+@[category research open, AMS 11]
+theorem erdos_409.variants.sigma.parts.i.isBigO (c : ℕ → ℕ)
+    (h : ∀ n > 1, IsLeast { i | (σ 1 · - 1)^[i] n |>.Prime } (c n)) :
+    (fun n => (c n : ℝ)) =O[atTop] (answer(sorry) : ℕ → ℝ) :=
+  sorry
+
+/--
+Let $c(n)$ be the minimum number of iterations of $n\mapsto\sigma(n) - 1$ before a prime
+is reached. Find the simplest function $g(n)$ such that $c(n) = o(g(n))$?
+-/
+@[category research open, AMS 11]
+theorem erdos_409.variants.sigma.parts.i.isLittleO (c : ℕ → ℕ)
+    (h : ∀ n > 1, IsLeast { i | (σ 1 · - 1)^[i] n |>.Prime } (c n)) :
+    (fun n => (c n : ℝ)) =o[atTop] (answer(sorry) : ℕ → ℝ) :=
   sorry
 
 /--
 Can infinitely many $n$ reach the same prime under the iteration $n\mapsto\sigma(n) - 1$?
 -/
 @[category research open, AMS 11]
-theorem erdos_409.variants.sigma.parts.ii
-    (iterEnd : ℕ → ℕ)
-    (iterEnd_spec: ∀ n, IsLeast { i | (fun j => (σ 1 j - 1 : ℕ))^[i] n |>.Prime } (iterEnd n)) :
-    ∃ (p : ℕ) (hp : p.Prime),
-      { n | (fun j => (j.totient - 1 : ℕ))^[iterEnd n] n = p }.Infinite :=
+theorem erdos_409.variants.sigma.parts.ii :
+    ∃ (p : ℕ) (hp : p.Prime), { n | ∃ i, (σ 1 · - 1)^[i] n = p }.Infinite :=
   sorry
 
 /--
 What is the density of $n$ which reach any fixed prime under the iteration $n\mapsto\sigma(n) - 1$?
 -/
 @[category research open, AMS 11]
-theorem erdos_409.variants.sigma.parts.iii (p : ℕ)
-    (h : p.Prime)
-    (iterEnd : ℕ → ℕ)
-    (iterEnd_spec: ∀ n, IsLeast { i | (fun j => (σ 1 j - 1 : ℕ))^[i] n |>.Prime } (iterEnd n))
-    (α : ℝ)
-    (hα : { n | (fun j => (j.totient - 1 : ℕ))^[iterEnd n] n = p }.HasDensity α) :
+theorem erdos_409.variants.sigma.parts.iii (p : ℕ) (h : p.Prime) (α : ℝ)
+    (hα : { n | ∃ i, (σ 1 · - 1)^[i] n = p }.HasDensity α) :
     α = answer(sorry) :=
   sorry

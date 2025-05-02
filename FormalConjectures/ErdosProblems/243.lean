984a58234dd15f5f2aad9ec031862cdea3c1bd34
Calle Sönne <csonne@google.com>
2025-05-02 19:11:57 +0200

    Erdos 243
    PiperOrigin-RevId: 754046108
    Change-Id: I196480701a56e1597d106c88912e5abf31d529d0

---
+++ FormalConjectures/ErdosProblems/243.lean
@@ -0,0 +1,38 @@
+/-
+Copyright 2025 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Erdős Problem 243
+
+*Reference:* [erdosproblems.com/243](https://www.erdosproblems.com/243)
+-/
+
+open Filter
+
+open scoped Topology
+
+/--
+Let $a_1 < a_2 < \dots$ be a sequence of integers such that $\lim_{n\to\infty} \frac{a_n}{a_{n-1}^2} = 1$ and $\sum \frac{1}{a_n} \in \mathbb{Q}$.
+
+Then, for all sufficiently large $n \ge 1$, $a_n = a_{n-1}^2 - a_{n-1} + 1$.
+-/
+@[category research open, AMS 40]
+theorem erdos_243 (a : ℕ → ℕ) (ha₀ : StrictMono a)
+    (ha₁ : Tendsto (fun n ↦ (a n : ℝ) / a (n - 1) ^ 2) atTop (𝓝 1))
+    (ha₂ : Summable ((1 : ℚ) / a ·)) :
+      ∀ᶠ n in atTop, a n = a (n - 1) ^ 2 - a (n - 1) + 1 := sorry

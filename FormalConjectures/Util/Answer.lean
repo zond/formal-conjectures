54f7919c9fe8b6b6ad93dd4dc8264ef579000e01
Paul Lezeau <lezeau@google.com>
2025-05-27 14:38:20 +0000

    Add some documentation to the answer elaborator

--- FormalConjectures/Util/Answer.lean
+++ FormalConjectures/Util/Answer.lean
@@ -23,6 +23,11 @@ import Batteries.Lean.Expr
 # The `answer( )` elaborator
 
 This file provides syntax for marking up answers in a problem statement.
+
+Note: certain problems also require guessing an answer, and can be formalised
+using `answer(sorry)` as a placeholder. In that case,
+**solving the problem is not the same as providing an answer and a proof**:
+the answer provided must also be mathematically meaningful.
 -/
 namespace Google
 

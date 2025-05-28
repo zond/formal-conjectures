700709b03280c772d63736be4b8f25004e27dd1a
Bhavik Mehta <bhavikmehta8@gmail.com>
2025-05-28 16:49:35 +0100

    Correct conditional version of Erdos 418 (#56)
    * goldbach fix
    
    * Update FormalConjectures/ErdosProblems/418.lean

--- FormalConjectures/ErdosProblems/418.lean
+++ FormalConjectures/ErdosProblems/418.lean
@@ -33,15 +33,33 @@ theorem erdos_418 : { (n - n.totient : ℕ) | n }ᶜ.Infinite :=
   sorry
 
 /--
-It follows from the Goldbach conjecture that every odd number can be
+It follows from a slight strengthening of the Goldbach conjecture that every odd number can be
 written as $n - \phi(n)$.
+In particular, we assume that every even number greater than 6 can be written as the sum of two
+*distinct* primes, in contrast to the usual Goldbach conjecture that every even number greater than
+2 can be written as the sum of two primes.
 -/
 @[category research solved, AMS 11]
 theorem erdos_418.variants.conditional
-    (goldbach : ∀ (n : ℕ), 2 < n → Even n → ∃ p q, Prime p ∧ Prime q ∧ n = p + q)
+    (goldbach : ∀ (n : ℕ), 6 < n → Even n → ∃ p q, p ≠ q ∧ p.Prime ∧ q.Prime ∧ n = p + q)
     (m : ℕ) (h : Odd m) :
     ∃ n, m + n.totient = n := by
-  sorry
+  obtain rfl | rfl | rfl | h7m : m = 1 ∨ m = 3 ∨ m = 5 ∨ 7 ≤ m := by
+    obtain ⟨m, rfl⟩ := h
+    omega
+  · exact ⟨2, rfl⟩
+  · exact ⟨9, rfl⟩
+  · exact ⟨25, rfl⟩
+  obtain ⟨p, q, hpq, hp, hq, hm⟩ := goldbach (m + 1) (by omega) (by simpa [parity_simps])
+  use p * q
+  have h2p : 2 ≤ p := hp.two_le
+  have h2q : 2 ≤ q := hq.two_le
+  rw [Nat.totient_mul, Nat.totient_prime hp, Nat.totient_prime hq]
+  · obtain ⟨p, rfl⟩ := le_iff_exists_add'.1 h2p
+    obtain ⟨q, rfl⟩ := le_iff_exists_add'.1 h2q
+    simp only [Nat.add_one_sub_one]
+    linear_combination hm
+  rwa [Nat.coprime_primes hp hq]
 
 /--
 Erdős has shown that a positive density set of integers cannot be written as

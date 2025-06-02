00fc218323fcd1b78c9a7b5f1b7e152ef7c8fece
Reklle <53151326+Reklle@users.noreply.github.com>
2025-06-02 22:45:16 +0300

    Add Brocard's Conjecture (#126)
    Co-authored-by: Paul Lezeau <paul.lezeau@gmail.com>
    Co-authored-by: Paul Lezeau <lezeau@google.com>

---
+++ FormalConjectures/Wikipedia/Brocard.lean
@@ -0,0 +1,35 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Brocard's Conjecture
+
+*Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Brocard%27s_conjecture)
+-/
+
+/--
+For every `n ≥ 2`, between the squares of the `n`-th and `(n+1)`-th primes,
+there are at least four prime numbers.
+-/
+@[category research open, AMS 11]
+theorem brocard_conjecture (n : ℕ) (hn : n ≥ 1) :
+    letI prev := n.nth Prime;
+    letI next := (n+1).nth Prime;
+    letI primesInBetween := (Finset.Ioo (prev^2) (next^2)).filter Nat.Prime
+    4 ≤ primesInBetween.card := by
+  sorry

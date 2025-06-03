0018c89a56d22c8ba2a461d64406223d49a699ad
Reklle <53151326+Reklle@users.noreply.github.com>
2025-06-03 11:28:11 +0300

    Add conjectures about Fermat numbers (#141)
    Co-authored-by: Calle Sönne <calle.sonne@gmail.com>
    Co-authored-by: Paul Lezeau <lezeau@google.com>

---
+++ FormalConjectures/Wikipedia/Fermat.lean
@@ -0,0 +1,51 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+/-!
+# Open questions about Fermat numbers
+
+*Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Fermat_number)
+-/
+
+/--
+Are Fermat numbers composite for all `n > 4`?
+-/
+@[category research open, AMS 11]
+theorem fermat_number_are_composite : (∀ n > 4, ¬Prime n.fermatNumber) ↔ answer(sorry) := by
+  sorry
+
+/--
+Are there infinitely many Fermat primes?
+-/
+@[category research open, AMS 11]
+theorem infinite_fermat_primes : Infinite {n : ℕ | Prime n.fermatNumber} ↔ answer(sorry) := by
+  sorry
+
+/--
+Are there infinitely many composite Fermat numbers?
+-/
+@[category research open, AMS 11]
+theorem infinite_fermat_composite : Infinite {n : ℕ | ¬Prime n.fermatNumber} ↔ answer(sorry) := by
+  sorry
+
+/--
+Are all Fermat numbers are square-free?
+-/
+@[category research open, AMS 11]
+theorem all_fermat_squarefree : (∀ (n : ℕ), Squarefree n.fermatNumber) ↔ answer(sorry) := by
+  sorry

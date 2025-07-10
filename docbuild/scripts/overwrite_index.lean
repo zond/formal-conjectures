zond/formal-conjectures

commit e5cf38b732f1996a47c110e2b12edcb04839b53a

Add file growth plot to documentation front page (#293)
- Script for generating plotly graph counting number of files per commit
since release date `docbuild/scripts/file_growth.py`
- Modify `docbuild/scripts/overwrite_index.lean` to read in generated
graph and inject to the statistics page in the docs
- Add script generation job to GitHub workflow before doc homepage is
overwritten

docbuild/scripts/overwrite_index.lean
@@ -107,13 +107,15 @@ unsafe def runWithImports {α : Type} (actionToRun : CoreM α) : IO α := do
 unsafe def main (args : List String) : IO Unit := do
   let .some file := args.get? 0
     | IO.println "Usage: stats <file>
-overwrites the contents of the `main` tag of a html `file` with a weclome page including stats."
+overwrites the contents of the `main` tag of a html `file` with a welcome page including stats."
   let inputHtmlContent ← IO.FS.readFile file
+  let .some graphFile := args.get? 1
+    | IO.println "Repository growth graph not supplied, generating docs without graph."
+  let graphHtml ← IO.FS.readFile graphFile
 
   runWithImports do
     let categoryStats ← getCategoryStatsMarkdown
     let subjectStats ← getSubjectStatsMarkdown
-
     let markdownBody :=
       s!"# Welcome to the *Formal Conjectures* Documentation!
 
@@ -130,14 +132,18 @@ classifications, please refer to the
 
 ## Problem Category Statistics
 {categoryStats}
-
 (note the links above use GitHub search, and so require logging into GitHub)
 
 ---
 
 ## Subject Category Statistics
-{subjectStats}"
+{subjectStats}
+
+---
+
+## Repository growth
+"
     IO.println markdownBody
     let .some newBody := MD4Lean.renderHtml (parserFlags := MD4Lean.MD_FLAG_TABLES ) markdownBody | throwError "Parsing failed"
-    let finalHtml ← replaceTag "main" inputHtmlContent newBody
+    let finalHtml ← replaceTag "main" inputHtmlContent (newBody ++ graphHtml)
     IO.FS.writeFile file finalHtml

docbuild/scripts/plot_growth.py
@@ -1,0 +1,89 @@
+import numpy as np
+import os
+import pandas as pd
+import plotly.express as px
+import re
+import subprocess
+from datetime import datetime, timedelta
+
+conf = {
+  'line_color': '#4285F4', # Color used in plotting
+  'line_width': 3, # Width of line used in the plot
+  'title': dict(
+    text='Number of files in Formal Conjectures', # Title text
+    font=dict(size=30), # Title font
+    x=0.5, # Center title
+    xanchor='center' # Center title
+  ),
+  'start_date': '2025-05-28', # Announcement date is '2025-05-28'
+  'xlabel': 'Date', # x-axis label on plot
+  'ylabel': 'Number of files', # y-axis label on plot
+  'out_path': 'docbuild/out/file_counts.html' # This is used as an arg to overwrite_index.lean in .github/workflows/build-and-docs.yml
+}
+
+def get_file_counts_over_time(start_date, columns):
+    """
+    Retrieves file counts over time
+
+    Args:
+        start_date (str): Date from which to start collecting commits
+        columns (list[str]): Column labels for the returned df, should of length 2
+
+    Returns:
+        pd.DataFrame: A DataFrame with dates as the first column and file counts as the second
+    """
+    if not isinstance(columns, list) or len(columns) != 2:
+      raise ValueError("The `columns` parameter should be a list of length 2.")
+
+    data = []
+
+    command = ['git', 'log',  '--pretty=format:%H,%ct']
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
+    commit_lines = result.stdout.strip().split('\n')
+
+    # Filter out any empty lines
+    commit_lines = [line for line in commit_lines if line]
+
+    # Process commits
+    for line in commit_lines:
+        # Extract sha and timestamp
+        sha, timestamp = line.split(',')
+        timestamp = int(timestamp)
+        # Only timestamps from `start_date`
+        if timestamp > datetime.fromisoformat(start_date).timestamp():
+          # Get the number of files in the current commit's tree
+          tree_command = ['git', 'ls-tree', '-r', '--name-only', sha]
+          tree_result = subprocess.run(tree_command, capture_output=True, text=True, check=True)
+          files = tree_result.stdout.strip().split('\n')
+          # Only care about files in `FormalConjectures` subdir
+          subdir_pattern = re.compile(r'^FormalConjectures/')
+          file_count = len([f for f in files if f and subdir_pattern.match(f)])
+          data.append([datetime.fromtimestamp(timestamp), file_count])
+
+    return pd.DataFrame(data, columns=columns)
+
+def plot_file_counts(df, xlabel, ylabel, line_color, line_width, title, out_path):
+    """
+    Plots the number of files over time.
+
+    Args:
+        df (pd.DataFrame): A pandas DataFrame which should contain `xlabel` and `ylabel` as columns
+        xlabel (str): The column from `df` to use as the `x`-axis
+        ylabel (str): The column from `df` to use as the `y`-axis
+        line_color (str): Colour of plotted graph
+        title (dict): Dictionary specifying graph title and style
+        out_path (str): Save location of html
+    """
+    fig = px.line(df, xlabel, ylabel)
+    fig.update_layout(title=title)
+    fig.update_traces(line_color=line_color, line_width=line_width)
+    fig.write_html(out_path, full_html=False, include_plotlyjs='cdn')
+
+if __name__ == "__main__":
+    github_url = "https://github.com/google-deepmind/formal-conjectures"
+    print(f"Generating growth plots for: {github_url}")
+
+    columns = [conf['xlabel'], conf['ylabel']]
+    df = get_file_counts_over_time(conf['start_date'], columns)
+    plot_file_counts(df, conf['xlabel'], conf['ylabel'], conf['line_color'], conf['line_width'],
+      conf['title'], conf['out_path'])

.github/workflows/build-and-docs.yml
@@ -107,16 +107,30 @@ jobs:
           mkdir out
           lake query FormalConjectures:docs > out-files.txt
           sed -i 's|^.*\.lake/build/doc/||' out-files.txt
-          echo "::group::Generated files" 
+          echo "::group::Generated files"
           cat out-files.txt
           echo "::endgroup::"
           rsync -a --files-from=out-files.txt --relative .lake/build/doc ./out
+
+      - name: Set up Python
+        uses: actions/setup-python@v5
+        with:
+          python-version: '3.12.9'
+
+      - name: Install Python dependencies
+        run: |
+          python -m pip install --upgrade pip
+          pip install pandas==2.2.3 numpy==2.2.3 plotly==5.20.0
+
+      - name: Run plotting script
+        run: |
+          python docbuild/scripts/plot_growth.py
 
       - name: Inject stats into index.html
         shell: bash
         run: |
           cd docbuild
-          lake exe overwrite_index ./out/index.html | tee -a "$GITHUB_STEP_SUMMARY"
+          lake exe overwrite_index ./out/index.html ./out/file_counts.html | tee -a "$GITHUB_STEP_SUMMARY"
 
       - name: Upload docs
         id: deployment

commit 728fa6539afdd7feaa667ebee69790d194552ec5

doc: fix styling issues for the growth plot on documentation page (#345)
- Fix bug introduced in #293 where a line was deleted causing the line
"(note the links above use GitHub search, and so require logging into
GitHub)" to be rendered as a row in the table above it
- Only count `*.lean` files in `FormalConjectures` directory (e.g., a
`README.md` file was introduced in #285)
- Specify sizing of plot to fix aspect ratio
- Reduce plot title font size

docbuild/scripts/overwrite_index.lean
@@ -132,6 +132,7 @@ classifications, please refer to the
 
 ## Problem Category Statistics
 {categoryStats}
+
 (note the links above use GitHub search, and so require logging into GitHub)
 
 ---

docbuild/scripts/plot_growth.py
@@ -11,10 +11,12 @@ conf = {
   'line_width': 3, # Width of line used in the plot
   'title': dict(
     text='Number of files in Formal Conjectures', # Title text
-    font=dict(size=30), # Title font
+    font=dict(size=15), # Title font
     x=0.5, # Center title
     xanchor='center' # Center title
   ),
+  'width': 1000, # Width of plot in px
+  'height': 500, # Height of plot in px
   'start_date': '2025-05-28', # Announcement date is '2025-05-28'
   'xlabel': 'Date', # x-axis label on plot
   'ylabel': 'Number of files', # y-axis label on plot
@@ -55,14 +57,16 @@ def get_file_counts_over_time(start_date, columns):
           tree_command = ['git', 'ls-tree', '-r', '--name-only', sha]
           tree_result = subprocess.run(tree_command, capture_output=True, text=True, check=True)
           files = tree_result.stdout.strip().split('\n')
-          # Only care about files in `FormalConjectures` subdir
-          subdir_pattern = re.compile(r'^FormalConjectures/')
+
+          # Only care about lean files in `FormalConjectures` subdir
+          subdir_pattern = re.compile(r'^FormalConjectures/.*\.lean')
+
           file_count = len([f for f in files if f and subdir_pattern.match(f)])
           data.append([datetime.fromtimestamp(timestamp), file_count])
 
     return pd.DataFrame(data, columns=columns)
 
-def plot_file_counts(df, xlabel, ylabel, line_color, line_width, title, out_path):
+def plot_file_counts(df, xlabel, ylabel, height, width, line_color, line_width, title, out_path):
     """
     Plots the number of files over time.
 
@@ -75,7 +79,7 @@ def plot_file_counts(df, xlabel, ylabel, line_color, line_width, title, out_path
         out_path (str): Save location of html
     """
     fig = px.line(df, xlabel, ylabel)
-    fig.update_layout(title=title)
+    fig.update_layout(title=title, height=height, width=width, autosize=False)
     fig.update_traces(line_color=line_color, line_width=line_width)
     fig.write_html(out_path, full_html=False, include_plotlyjs='cdn')
 
@@ -85,5 +89,14 @@ if __name__ == "__main__":
 
     columns = [conf['xlabel'], conf['ylabel']]
     df = get_file_counts_over_time(conf['start_date'], columns)
-    plot_file_counts(df, conf['xlabel'], conf['ylabel'], conf['line_color'], conf['line_width'],
-      conf['title'], conf['out_path'])
+    plot_file_counts(
+      df=df,
+      xlabel=conf['xlabel'],
+      ylabel=conf['ylabel'],
+      height=conf['height'],
+      width=conf['width'],
+      line_color=conf['line_color'],
+      line_width=conf['line_width'],
+      title=conf['title'],
+      out_path=conf['out_path']
+    )
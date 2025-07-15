55b5ce1c01823ab43aeacb53d8f0aa8eaf6ab22f
Moritz Firsching <firsching@google.com>
2025-07-15 16:58:10 +0200

    Variant for infinitude of Fibonacci primes. (#355)
    Also restructure documentation a bit.
    
    ---------
    
    Co-authored-by: Calle Sönne <calle.sonne@gmail.com>

--- FormalConjectures/Wikipedia/FibonacciPrimes.lean
+++ FormalConjectures/Wikipedia/FibonacciPrimes.lean
@@ -16,13 +16,46 @@ limitations under the License.
 
 import FormalConjectures.Util.ProblemImports
 
+/-!
+# Fibonacci Primes
+
+*Reference:* [Wikipedia](https://en.wikipedia.org/wiki/Fibonacci_prime)
+-/
+
 /--
-This conjecture states that there are infinitely many Fibonacci primes
-(i.e., Fibonacci numbers that are prime). It is unsolved. It is
-stated on Wikipedia here: https://en.wikipedia.org/wiki/Fibonacci_prime.
+There are infinitely many Fibonacci primes, i.e., Fibonacci numbers that are prime
 It is also a barrier to defining a benchmark from this paper:
 https://arxiv.org/html/2505.13938v1 (see Figure 8).
+
 -/
 @[category research open, AMS 11]
-theorem fib_primes_infinite : {n : ℕ | n.fib.Prime}.Infinite := by
+theorem fib_primes_infinite : {n : ℕ | (∃ m : ℕ, m.fib = n) ∧ n.Prime}.Infinite := by
   sorry
+
+/--
+There are infinitely many indices $i$, such that the $i$-th Fibonacci is prime.
+-/
+@[category research open, AMS 11]
+theorem fib_primes_infinite.variant : {n : ℕ | n.fib.Prime}.Infinite := by
+  sorry
+
+/--
+The two ways of phrasing the conjecture are equivalent.
+-/
+@[category test, AMS 11]
+theorem indices_infinite_iff_fib_primes_infinite : type_of% fib_primes_infinite.variant ↔
+    type_of% fib_primes_infinite := by
+  simp only [Set.infinite_iff_exists_gt]
+  constructor
+  · intros h a
+    rcases h (a + 1) with ⟨b', ⟨hb₁, hb₂⟩⟩
+    use Nat.fib b'
+    exact ⟨⟨exists_apply_eq_apply Nat.fib b', hb₁⟩,
+      by linear_combination b'.le_fib_add_one + hb₂⟩
+  · intro h a
+    rcases h (Nat.fib a) with ⟨b', ⟨⟨m, hm⟩, hb₁⟩, hb₂⟩
+    use m
+    constructor
+    · simp_all
+    · have := @m.fib_mono a
+      omega

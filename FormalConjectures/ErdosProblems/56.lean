56df2d651c99a34e49d9f6e30964b9958f218a2d
Paul Lezeau <lezeau@google.com>
2025-08-13 17:40:50 +0100

    fix: brackets in erdos_56 (#562)

--- FormalConjectures/ErdosProblems/56.lean
+++ FormalConjectures/ErdosProblems/56.lean
@@ -108,6 +108,6 @@ relatively prime. An example is the set of all multiples of the first $k$ primes
 Is this the largest such set?
 -/
 @[category research solved, AMS 11]
-theorem erdos_56 : ∀ᵉ (N ≥ 2) (k > 0), (MaxWeaklyDivisible N k = (FirstPrimesMultiples N k).card) ↔
+theorem erdos_56 : (∀ᵉ (N ≥ 2) (k > 0), (MaxWeaklyDivisible N k = (FirstPrimesMultiples N k).card)) ↔
     answer(False) := by
   sorry

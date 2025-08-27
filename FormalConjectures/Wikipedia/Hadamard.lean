2308a4ce9eebaf9fbae1d0036ac43a4aec61cd37
Moritz Firsching <firsching@google.com>
2025-08-27 10:39:26 +0200

    variant for solved cases of Hadamard conjecture (#619)

--- FormalConjectures/Wikipedia/Hadamard.lean
+++ FormalConjectures/Wikipedia/Hadamard.lean
@@ -116,10 +116,18 @@ example : IsHadamard H12 := by
   sorry
 
 /--
+For all $k ≤ 166$, it is known there that there is a Hadamard matrix of size $4 * k$.
+-/
+@[category research solved, AMS 15]
+theorem HadamardConjecture.variants.first_cases (k : ℕ) (h : k ≤ 166) :
+    ∃ M, IsHadamard (n := 4 * k) M := by
+  sorry
+
+/--
 The smallest order for which no Hadamard matrix is presently known is $668 = 4 * 167$.
 -/
 @[category research open, AMS 15]
-theorem HadamardConjecture.variant : ∃ M, IsHadamard (n := 4 * 167) M := by
+theorem HadamardConjecture.variants.«167» : ∃ M, IsHadamard (n := 4 * 167) M := by
   sorry
 
 end Hadamard

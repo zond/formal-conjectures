ab112fac2241f67f366332bb58fcf4105885173c
zhenningdavidliu <zhenninghimme@gmail.com>
2025-08-27 17:59:27 +0200

    Erdős problem 41 (#615)
    Pull request for issue 219
    (https://github.com/google-deepmind/formal-conjectures/issues/219)
    
    ---------
    
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Paul Lezeau <lezeau@google.com>
    Co-authored-by: Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>

---
+++ FormalConjectures/ErdosProblems/41.lean
@@ -0,0 +1,55 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+
+variable {α : Type} [AddCommMonoid α]
+
+/-!
+# Erdős Problem 41
+
+*Reference:* [erdosproblems.com/41](https://www.erdosproblems.com/41)
+-/
+
+open Classical
+
+/--
+For a given set `A`, the n-tuple sums `a₁ + ... + aₙ` are all distinct for  `a₁, ..., aₙ` in `A` (aside from the trivial coincidences).
+-/
+def NtupleCondition (A : Set α) (n : ℕ) : Prop := ∀ (I : Finset α) (J : Finset α),
+  I.toSet ⊆ A ∧ J.toSet ⊆ A ∧ I.card = n ∧ J.card = n ∧
+  (∑ i ∈ I, i = ∑ j ∈ J, j) → I = J
+
+/-- Given a set of natural numbers `A`, `Set.bdd A N` is the set `{1,...,N} ∩ A`-/
+private noncomputable def Set.bdd (A : Set ℕ) (N : ℕ) : Finset ℕ :=
+    Finset.Icc 1 N |>.filter (· ∈ A)
+
+/--
+Let `A ⊆ ℕ` be an infinite set such that the triple sums `a + b + c` are all distinct for `a, b, c` in `A` (aside from the trivial coincidences). Is it true that `lim inf n → ∞ |A ∩ {1, …, N}| / N^(1/3) = 0`?
+-/
+@[category research open, AMS 11]
+theorem erdos_41 (A : Set ℕ) (h_triple : NtupleCondition A 3) (h_infinite : A.Infinite) : 
+    Filter.atTop.liminf (fun N => (A.bdd N).card / (N : ℝ)^(1/3)) = 0 := by
+  sorry
+
+/--
+Erdős proved the following pairwise version.
+Let `A ⊆ ℕ` be an infinite set such that the pairwise sums `a + b` are all distinct for `a, b` in `A` (aside from the trivial coincidences). Is it true that `lim inf n → ∞ |A ∩ {1, …, N}| / N^(1/2) = 0`?
+-/
+@[category research solved, AMS 11]
+theorem erdos_41_i (A : Set ℕ) (h_pair : NtupleCondition A 2) (h_infinite : A.Infinite) : 
+    Filter.atTop.liminf (fun N => (A.bdd N).card / (N : ℝ).sqrt) = 0 := by
+  sorry

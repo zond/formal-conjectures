a5e527f06a22f0a6b0f4446fbb65576b641548f1
Moritz Firsching <firsching@google.com>
2025-09-08 10:13:27 +0200

    feat: `Cocolorable` and `cochromaticNumber` (#661)
    Co-authored-by: Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>

--- FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Coloring.lean
+++ FormalConjectures/ForMathlib/Combinatorics/SimpleGraph/Coloring.lean
@@ -13,9 +13,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 -/
-import Mathlib
+import Mathlib.Combinatorics.SimpleGraph.Coloring
+import Mathlib.Order.CompletePartialOrder
 
 universe u
+
+open Classical
 
 variable {V : Type u} (G : SimpleGraph V)
 
@@ -52,3 +55,31 @@ A graph `G` is `k`-critical (or vertex-critical) if its chromatic number is `k`,
 and deleting any single vertex reduces the chromatic number.
 -/
 def IsCritical (k : ℕ) : Prop := G.chromaticNumber = k ∧ ∀ v, (⊤ : G.Subgraph).IsCritialVertex v
+
+
+open SimpleGraph
+
+theorem colorable_iff_induce_eq_bot (G : SimpleGraph V) (n : ℕ) :
+    G.Colorable n ↔ ∃ coloring : V → Fin n, ∀ i, G.induce {v | coloring v = i} = ⊥ := by
+  refine ⟨fun ⟨a, h⟩ ↦ ⟨a, by aesop⟩, fun ⟨w, h⟩ ↦ ⟨w, @fun a b h_adj ↦ ?_⟩⟩
+  specialize h (w a)
+  contrapose h
+  intro hG
+  have : ¬ ((SimpleGraph.induce {v | w v = w a} G).Adj ⟨a, by rfl⟩ ⟨b, by simp_all⟩) :=
+    hG ▸ fun a ↦ a
+  exact this h_adj
+
+def Cocolorable (G : SimpleGraph V) (n : ℕ) : Prop := ∃ coloring : V → Fin n,
+  ∀ i, G.induce {v | coloring v = i} = ⊥ ∨ G.induce {v | coloring v = i} = ⊤
+
+example (G : SimpleGraph V) (n : ℕ) : G.Colorable n → SimpleGraph.Cocolorable G n := by
+  simp [colorable_iff_induce_eq_bot, Cocolorable]
+  aesop
+
+/--
+The chromatic number of a graph is the minimal number of colors needed to color it.
+This is `⊤` (infinity) iff `G` isn't colorable with finitely many colors.
+
+If `G` is colorable, then `ENat.toNat G.chromaticNumber` is the `ℕ`-valued chromatic number.
+-/
+noncomputable def cochromaticNumber (G : SimpleGraph V) : ℕ∞ := ⨅ n ∈ setOf G.Cocolorable, (n : ℕ∞)
